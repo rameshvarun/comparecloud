5a2a069a5d969fb1e7befccabd2c3650eeb4cd74
Varun Ramesh <varunramesh@live.com>
2018-09-02 05:40:24 -0700

    Allow jsx

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-interface Provider {
-    readonly name: string;
-    readonly url: string;
-
-    getMonthlyPrice: (storage: number) => number;
-    getYearlyPrice?: (storage: number) => number;
-
-
-}
---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+// interface Provider {
+//     readonly name: string;
+//     readonly url: string;
+//
+//     getMonthlyPrice: (storage: number) => number;
+//     getYearlyPrice?: (storage: number) => number;
+//
+//
+// }
+
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
